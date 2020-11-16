0145279db1a41d7756a8d08b55dd377949b0c173
Curtis White <drizztguen77@gmail.com>
2020-11-15 22:46:19 -0700

    Enhanced SimpleAxis example

--- examples/SimpleAxis.py
+++ examples/SimpleAxis.py
@@ -18,9 +18,10 @@ def show_responses(axis):
         print("No responses received")
 
 
-steps_per_rev = int(input("How many steps per revolution [400]? ") or "400")
-total_revolutions = int(input("How many total revolutions [20]? ") or "20")
-rpm = int(input("How many RPMs [200]? ") or "200")
+steps_per_rev = int(input("How many steps per revolution [1600]? ") or "1600")
+total_revolutions = int(input("How many total revolutions [50]? ") or "50")
+rpm = int(input("How many RPMs [500]? ") or "500")
+direction = int(input("Direction (Forward = 0, Reverse = 1) [0]") or "0")
 
 xaxis = Axis("X")
 xaxis.command_id = 1
@@ -30,7 +31,7 @@ xaxis.serial_device = "/dev/ttyS0"
 # Setup the axis with values to start the motor
 xaxis.frequency = xaxis.rpm_to_frequency(rpm=rpm, steps_per_rev=steps_per_rev, round_digits=3)
 xaxis.pulse_count = xaxis.calculate_pulse_count(steps_per_rev, total_revolutions)
-xaxis.direction = 0                 # clockwise
+xaxis.direction = direction
 xaxis.start_ramp = 1                # ramp up
 xaxis.finish_ramp = 1               # ramp down
 xaxis.ramp_divide = 100             # divide frequency by this for ramp increment
@@ -45,6 +46,10 @@ responses = xaxis.get_all_responses()
 while not all(x in responses for x in ["RI01CX*", "CI01CX*"]):
     responses = responses + xaxis.get_all_responses()
 
+# Print the responses
+print(f"------- Set axis command responses -------")
+xaxis.parse_responses(responses)
+
 # Start the motor
 xaxis.send_command(xaxis.start())
 
@@ -52,6 +57,10 @@ xaxis.send_command(xaxis.start())
 responses = xaxis.get_all_responses()
 while not all(x in responses for x in ["RI01SX*", "CI01SX*"]):
     responses = responses + xaxis.get_all_responses()
+
+# Print the responses
+print(f"------- Start command responses -------")
+xaxis.parse_responses(responses)
 
 # Get the pulse count
 xaxis.send_command(xaxis.get_current_pulse_count())
@@ -63,4 +72,5 @@ while not all(x in responses for x in ["RI01XP*", pulse_reply, "CI01XP*"]):
     responses = responses + xaxis.get_all_responses()
 
 # Print the responses
+print(f"------- Get pulse count command responses -------")
 xaxis.parse_responses(responses)
