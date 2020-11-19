671e5c6cfe4e795fce6dc2a59bb12bcff5b372f8
Curtis White <drizztguen77@gmail.com>
2020-11-19 09:59:18 -0700

    Fixed a couple bugs

--- examples/ChangeSpeed.py
+++ examples/ChangeSpeed.py
@@ -21,6 +21,7 @@ def show_responses(axis):
 steps_per_rev = int(input("How many steps per revolution [1600]? ") or "1600")
 direction = 0
 rpm = 300
+pulse_count = 4294967295    # Set to max so we can start and stop it when desired
 
 xaxis = Axis("X", command_id=1, serial_device="/dev/ttyS0")
 xaxis.debug = True
@@ -56,28 +57,32 @@ xaxis.parse_responses(responses)
 for x in range(3):
     time.sleep(3)
 
-    new_frequency = xaxis.rpm_to_frequency(rpm=rpm + 100, steps_per_rev=steps_per_rev, round_digits=3)
-    xaxis.change_speed(new_frequency)
+    rpm = rpm + 100
+    new_frequency = xaxis.rpm_to_frequency(rpm=rpm, steps_per_rev=steps_per_rev, round_digits=3)
+    xaxis.send_command(xaxis.change_speed(new_frequency))
 
     # Check for both reply and complete responses to be returned
     responses = xaxis.get_all_responses()
     while not all(x in responses for x in ["RI01QX*", "CI01QX*"]):
         responses = responses + xaxis.get_all_responses()
+    print(f"------- Speed changed to {rpm} - command responses -------")
 
 # Decrement the speed twice times by 100 rpm each time
 for x in range(2):
     time.sleep(3)
 
-    new_frequency = xaxis.rpm_to_frequency(rpm=rpm - 100, steps_per_rev=steps_per_rev, round_digits=3)
-    xaxis.change_speed(new_frequency)
+    rpm = rpm - 100
+    new_frequency = xaxis.rpm_to_frequency(rpm=rpm, steps_per_rev=steps_per_rev, round_digits=3)
+    xaxis.send_command(xaxis.change_speed(new_frequency))
 
     # Check for both reply and complete responses to be returned
     responses = xaxis.get_all_responses()
     while not all(x in responses for x in ["RI01QX*", "CI01QX*"]):
         responses = responses + xaxis.get_all_responses()
+    print(f"------- Speed changed to {rpm} - command responses -------")
 
 # Shut it all down
-xaxis.stop()
+xaxis.send_command(xaxis.stop())
 
 # Check for both reply and complete responses to be returned
 responses = xaxis.get_all_responses()
@@ -85,5 +90,5 @@ while not all(x in responses for x in ["RI01TX*", "CI01SX*"]):
     responses = responses + xaxis.get_all_responses()
 
 # Print the responses
-print(f"------- Start command responses -------")
+print(f"------- Stop command responses -------")
 xaxis.parse_responses(responses)
