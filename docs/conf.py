6db594c0cde723819d66da23fac41f328467b64c
Curtis White <drizztguen77@gmail.com>
2020-11-29 17:08:02 -0700

    Tried changing conf.py to see if I can get it to work in RTD

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../pthat/'))
+# sys.path.insert(0, os.path.abspath('../pthat/'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'pthat')))
 
 
 # -- Project information -----------------------------------------------------
